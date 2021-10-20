27a0e8ba89b0bbf48ee03ff481cfcdc8aab1d801
Mindslave <mindslave.blog@protonmail.com>
2021-10-20 17:35:59 +0200

    basic docker-compose setup

--- internal/warehouse/warehouse.go
+++ internal/warehouse/warehouse.go
@@ -1,6 +1,13 @@
 package warehouse
 
-import "github.com/mariuskimmina/supplywatch/pkg/logger"
+import (
+	"context"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/mariuskimmina/supplywatch/pkg/logger"
+)
 
 type warehouse struct {
 
@@ -10,6 +17,67 @@ func NewWarehouse() *warehouse {
     return &warehouse{}
 }
 
+var (
+    address = "0.0.0.0:1234"
+)
+
+const (
+    maxBufferSize = 1024
+)
+
 func (w *warehouse) Start() {
+    ctx := context.Background()
     logger.Debug("Starting warehouse")
+    listen, err := net.ListenPacket("udp", address)
+    if err != nil {
+        return
+    }
+
+    defer listen.Close()
+
+    doneChan := make(chan error, 1)
+    buffer := make([]byte, maxBufferSize)
+
+    go func() {
+        for {
+            n, addr, err := listen.ReadFrom(buffer)
+            if err != nil {
+                doneChan <- err
+                return
+            }
+
+            fmt.Printf("packet-received: bytes=%d from=%s\n",
+            n, addr.String())
+
+            writeDeadline := time.Now().Add(15 * time.Second)
+            err = listen.SetWriteDeadline(writeDeadline)
+            if err != nil {
+                doneChan <- err
+                return
+            }
+            readDeadline := time.Now().Add(15 * time.Second)
+            err = listen.SetReadDeadline(readDeadline)
+            if err != nil {
+                doneChan <- err
+                return
+            }
+
+            n, err = listen.WriteTo(buffer[:n], addr)
+            if err != nil {
+                doneChan <- err
+                return
+            }
+
+            fmt.Printf("packet-written: bytes=%d to=%s\n", n, addr.String())
+        }
+    }()
+
+    select {
+    case <-ctx.Done():
+        fmt.Println("cancelled")
+        err = ctx.Err()
+    case err = <-doneChan:
+    }
+
+    return
 }
