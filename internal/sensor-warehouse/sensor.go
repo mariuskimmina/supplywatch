bcb4cea9f2829d2451b4aa3f46f19e064ebe17bf
Marius Kimmina <marius.kimmina@protonmail.com>
2021-11-07 09:21:09 +0100

    better message format

--- internal/sensor-warehouse/sensor.go
+++ internal/sensor-warehouse/sensor.go
@@ -11,11 +11,20 @@ type Sensor struct {
 	logger *log.Logger
 }
 
+type Message struct {
+	SensorType 	string
+	MessageBody string
+}
+
 func NewSensor(logger *log.Logger) *Sensor {
 	return &Sensor{
 		logger: logger,
 	}
 }
+
+const (
+	sensorType = "Warehouse-sensor"
+)
 
 func (s *Sensor) Start() {
 	products := []string{
@@ -23,12 +32,16 @@ func (s *Sensor) Start() {
 		"Backpulver",
 	}
 	conn, err := net.Dial("udp", "supplywatch_warehouse_1:4444")
+	message := Message{
+		SensorType: sensorType,
+		MessageBody: products[0],
+	}
 	if err != nil {
 		s.logger.Error("Failed to dial")
 	}
 	for {
 		s.logger.Info("Sending Mehl")
-		conn.Write([]byte(products[0]))
+		conn.Write([]byte(message)
 		time.Sleep(5 * time.Second)
 	}
 }
